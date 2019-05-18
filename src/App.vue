8315a03efb3e2379099e6def0d877535aebf1b94
Roytangrb <roytang.rb@gmail.com>
2019-05-19 03:32:12 +0800

    add Footer

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Footer from './components/Footer'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Footer
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
+import '@fortawesome/fontawesome-free/css/all.css'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify, {
+  iconFont: 'fa',
+})
 
 new Vue({
   render: h => h(App),
