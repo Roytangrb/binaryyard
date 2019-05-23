5b9092676056f049a409bc4755d31c28eb697db7
Roytangrb <roytang.rb@gmail.com>
2019-05-24 03:24:36 +0800

    add message form control logics

--- README.md
+++ README.md
@@ -32,3 +32,7 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## TODO
+ - update privacy policy
+ - async validation of message sent to server
--- src/components/Enquiry.vue
+++ src/components/Enquiry.vue
@@ -3,32 +3,90 @@
     <v-container grid-list-xl>
       <v-layout
         wrap
-        justify-space-between
+        justify-center
       >
         <v-flex xs12 md4>
+          <!-- Input Fields -->
           <v-text-field
             v-model="model_name"
             placeholder="Your name: "
             :rules="rules_name"
             label="Name"
+            required
           ></v-text-field>
           <v-text-field
             v-model="model_email"
             placeholder="example@example.com"
             :rules="rules_email"
             label="Email"
+            required
           ></v-text-field>
         </v-flex>
 
         <v-flex xs12 md6>
-          <v-text-field
+          <v-textarea
             v-model="model_message"
             placeholder="Drop us a messge:"
             :counter="max"
             :rules="rules_message"
             label="Message"
-          ></v-text-field>
+            required
+          ></v-textarea>
         </v-flex>
+        <!-- Input Fields -->
+        <!-- Submission -->
+        <v-flex xs12>
+          <div class="text-xs-center">
+            <v-dialog
+              v-model="dialog"
+              width="500"
+              :disabled="!valid"
+            >
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  :color="valid? 'red lighten-2' : 'grey lighten-2'"
+                  dark
+                  v-on="on"
+                >
+                  Send
+                </v-btn>
+              </template>
+        
+              <v-card>
+                <v-card-title
+                  class="headline grey lighten-2"
+                  primary-title
+                >
+                  Privacy Policy
+                </v-card-title>
+        
+                <v-card-text>{{ dialogText }}</v-card-text>
+        
+                <v-divider></v-divider>
+        
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="secondary"
+                    flat
+                    @click="back"
+                  >
+                    Back
+                  </v-btn>
+                  <v-btn
+                    color="primary"
+                    flat
+                    @click="submit"
+                    :disabled="submitted"
+                  >
+                    {{submitButtonText}}
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </div>
+        </v-flex>
+        <!-- Submission -->
       </v-layout>
     </v-container>
   </v-form>
@@ -38,31 +96,36 @@
 export default {
   name: 'enquiryMessage',
   data: () => ({
-    match_email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    match_name: /^(?!\s*$).+/,
+    valid: false,
+    submitted: false,
+    dialog: false,
+    email_test: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    name_test: /^(?!\s*$).+/,
     max: 300,
     model_name: '',
     model_email: '',
-    model_message: ''
+    model_message: '',
+    submitButtonText: 'Send',
+    dialogText: 'Privacy Policy'
   }),
   computed: {
     rules_name () {
-      const rules = []
+      const rules = [v => !!v || 'Name is required']
 
       const rule = 
-        v => v.match(this.match_name) || 
+        v => this.name_test.test(v) || 
           'Name cannot be empty'
-      
+
       rules.push(rule)
 
       return rules
     },
 
     rules_email () {
-      const rules = []
+      const rules = [v => !!v || 'Email is required',]
 
       const rule =
-        v => v.match(this.match_email) ||
+        v => this.email_test.test(v) ||
           'Invalid Email Input'
 
       rules.push(rule)
@@ -71,7 +134,7 @@ export default {
     },
 
     rules_message () {
-      const rules = []
+      const rules = [v => !!v || 'Message is required',]
 
       const rule =
         v => (v || '').length <= this.max ||
@@ -84,16 +147,48 @@ export default {
   },
 
   watch: {
-    match_name: 'validateField',
-    match_email: 'validateField',
-    max: 'validateField',
+    model_name: 'validateField', 
     model_email: 'validateField',
-    model_message: 'validateField'
+    model_message: 'validateField',
+    dialog: "changeDialogText",
+    submitted: 'changeSubmittedStats'
   },
 
   methods: {
     validateField () {
-      this.$refs.form.validate()
+      if (this.$refs.form.validate()){
+        this.valid = true
+      }
+    },
+    submit () { 
+      //async callback
+      this.submitted = true
+    },
+    back() {
+      this.dialog = false
+      if (this.submitted){
+        this.reset()
+      }
+    },
+    /* change dialog text after click outside dialog*/
+    changeDialogText () {
+      if (this.dialog === false){
+        this.back()
+      }
+    },
+    changeSubmittedStats () {
+      if (this.submitted){
+        this.dialogText = 'You message has been sent out successfully! Thanks for connecting!'
+        this.submitButtonText = 'Sent Successfully!'
+      } else {
+        this.dialogText = 'Privacy Policy'
+        this.submitButtonText = 'Send'
+      }
+    },
+    reset () {
+      this.$refs.form.reset()
+      this.valid = false
+      this.submitted = false 
     }
   }
 }
