c990395a986801c17627053c1624101828861fe6
Roytangrb <roytang.rb@gmail.com>
2019-05-24 15:19:51 +0700

    v-layout bug fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,54 +7,56 @@
       width="100%"
       height="auto"
     >
-      <v-layout row>
-        <v-flex md4 sm12>
-          <v-card-text class="white--text text-xs-left">
-            <h1>Binaryyard <br> ...</h1>
-            <p>+852 1234 5678</p>
-            <p>1a, 99/F, Binaryyard Building
-            <br>Kowloon, Hong Kong</p>
-          </v-card-text>
-        </v-flex>
+      <v-container>
+        <v-layout row>
+          <v-flex md4 sm12>
+            <v-card-text class="white--text text-xs-left">
+              <h1>Binaryyard <br> ...</h1>
+              <p>+852 1234 5678</p>
+              <p>1a, 99/F, Binaryyard Building
+              <br>Kowloon, Hong Kong</p>
+            </v-card-text>
+          </v-flex>
 
-        <v-flex md4 class="hidden-sm-and-down">
-          <v-card-text class="white--text text-xs-left">
-            <p>About</p>
-            <p>Services</p>
-            <p>Projects</p>
-            <p>Workshops</p>
-          </v-card-text>
-        </v-flex>
-        <v-flex md4 class="hidden-sm-and-down">
-          <v-card-text class="white--text text-xs-left">
-            <p>About</p>
-            <p>Services</p>
-            <p>Projects</p>
-            <p>Workshops</p>
-          </v-card-text>
-        </v-flex>
-      </v-layout>
+          <v-flex md4 class="hidden-sm-and-down">
+            <v-card-text class="white--text text-xs-left">
+              <p>About</p>
+              <p>Services</p>
+              <p>Projects</p>
+              <p>Workshops</p>
+            </v-card-text>
+          </v-flex>
+          <v-flex md4 class="hidden-sm-and-down">
+            <v-card-text class="white--text text-xs-left">
+              <p>About</p>
+              <p>Services</p>
+              <p>Projects</p>
+              <p>Workshops</p>
+            </v-card-text>
+          </v-flex>
+        </v-layout>
 
-      <v-latout row>
-        <v-flex xs12>
-          <v-card-text class="text-xs-center">
-              <v-btn
-                v-for="icon in icons"
-                :key="icon.shape"
-                class="mx-3 white--text"
-                icon
-                :href="icon.href"
-              >
-              <v-icon size="24px">{{ icon.shape }}</v-icon>
-              </v-btn>
-          </v-card-text>
-        </v-flex>
-      </v-latout>
+        <v-layout row>
+          <v-flex xs12>
+            <v-card-text class="text-xs-center">
+                <v-btn
+                  v-for="icon in icons"
+                  :key="icon.shape"
+                  class="mx-3 white--text"
+                  icon
+                  :href="icon.href"
+                >
+                <v-icon size="24px">{{ icon.shape }}</v-icon>
+                </v-btn>
+            </v-card-text>
+          </v-flex>
+        </v-layout>
 
-      <v-divider></v-divider>
-      <v-card-text class="text-xs-center">
-        &copy;{{year}} - <strong>Binaryyard</strong>
-      </v-card-text>
+        <v-divider></v-divider>
+        <v-card-text class="text-xs-center">
+          &copy;{{year}} - <strong>Binaryyard</strong>
+        </v-card-text>
+      </v-container>
     </v-card>
   </v-footer>
 </template>
